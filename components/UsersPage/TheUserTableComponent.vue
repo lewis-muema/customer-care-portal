990df897e60facda07994954c200ec5d6f99900b
joyce.k <joyce@sendy.co.ke>
2019-07-18 17:24:18 +0300

    added the user table component which will hold the users table

---
+++ components/UsersPage/TheUserTableComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="content">
+    <div class="row">
+      <div class="box">
+        <div class="box-body">dfedf</div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: 'TheUserTableComponent',
+};
+</script>
