ab66e121c9c70d4d31912117bba3f0e67d46f32b
Daudi Muriu <daudi@sendyit.com>
2019-07-18 17:17:00 +0300

    Sample test to ensure that the components are being loaded and to know whether the test will run

--- test/add-test.js
+++
@@ -1,8 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import add from '@components/add.vue';
-
-const expect = require('chai').expect;
-
-const wrapper = mount(add);
-
-const vm = wrapper.vm;
