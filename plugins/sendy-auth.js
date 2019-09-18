27dc19dbdc2715397976be801af4daca7544ed45
joyce.k <joyce@sendy.co.ke>
2019-09-18 12:52:38 +0300

    hide url

--- plugins/sendy-auth.js
+++ plugins/sendy-auth.js
@@ -3,16 +3,10 @@ import Vue from 'vue';
 import SendyAuth from '@sendyit/auth';
 
 Vue.use(SendyAuth, {
-  // social authentication driver: 'google'
-  // default: google
+  authUrl: process.env.LOGIN_URL,
 
-  // internal authentication url
-  authUrl: 'https://authtest.sendyit.com/parcel/admin_glogin',
-
-  // custom configurations for social drivers
   configs: {
     google: {
-      // google's client key & identification for gapi
       clientId:
         '693530769708-t8ibd43rpavia989f7n95dqtcps5rt33.apps.googleusercontent.com',
     },
