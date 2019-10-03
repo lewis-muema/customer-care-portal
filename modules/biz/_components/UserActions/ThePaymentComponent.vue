9794ae1ad285863e720bcb2cb26b1a2864627be2
joyce.k <joyce@sendy.co.ke>
2019-10-03 12:27:04 +0300

    remove async where there is no await

--- modules/biz/_components/UserActions/ThePaymentComponent.vue
+++ modules/biz/_components/UserActions/ThePaymentComponent.vue
@@ -258,7 +258,7 @@ export default {
         this.updateErrors(notification);
       }
     },
-    async submitPayment() {
+    submitPayment() {
       const notification = [];
       const actionClass = '';
 
