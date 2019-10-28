acbba0a0e3e1012c503734e3dcffb64c3e4f3d82
joyce.k <joyce@sendy.co.ke>
2019-10-28 10:46:24 +0300

    redirect to orders page when the base url is given

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,5 @@
-<template> </template>
 <script>
-// import config from '~/config/configs';
-import { mapState } from 'vuex';
-
 export default {
-  data() {
-    return {
-      STRIPE_SECRET: process.env.STRIPE_SECRET,
-    };
-  },
-  computed: {
-    ...mapState(['config']),
-  },
-  mounted() {
-    console.log('STRIPE_SECRET', this.config);
-  },
+  middleware: ['redirect'],
 };
 </script>
