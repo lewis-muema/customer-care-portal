d4b185f6c3f80496d401aef47386013ef5e2556e
Daudi Muriu <daudi@sendyit.com>
2019-12-11 23:00:41 +0300

    Changed all tabs from being active

--- modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/DashboardComponent.vue
+++ modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/DashboardComponent.vue
@@ -1,5 +1,87 @@
 <template>
-  <div>
+  <div id="tabs" class="container">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <li class="nav-item">
+        <a
+          class="nav-link active"
+          :id="`total_${orderNo}`"
+          data-toggle="tab"
+          role="tab"
+          aria-selected="true"
+          @click="viewTab('total', orderNo)"
+          >Total</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :id="`assigned_${orderNo}`"
+          data-toggle="tab"
+          role="tab"
+          aria-selected="false"
+          @click="viewTab('assigned', orderNo)"
+          >Assigned</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :id="`unassigned_${orderNo}`"
+          data-toggle="tab"
+          role="tab"
+          aria-selected="false"
+          @click="viewTab('unassigned', orderNo)"
+          >Unassigned</a
+        >
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          :id="`create_${orderNo}`"
+          data-toggle="tab"
+          role="tab"
+          aria-selected="false"
+          @click="viewTab('create', orderNo)"
+          >Create</a
+        >
+      </li>
+    </ul>
+    <div>
+      <div>
+        <div
+          :class="`tab-pane fade ${firstShow} ${firstActive}`"
+          :id="`total_${orderNo}`"
+          role="tabpanel"
+          v-if="showTab === `total_${orderNo}`"
+        >
+          <AssignedComponent :order="order" :rates="conversionRates" />
+        </div>
+        <div
+          :class="`tab-pane fade ${show} ${active}`"
+          :id="`assigned_${orderNo}`"
+          role="tabpanel"
+          v-if="showTab === `assigned_${orderNo}`"
+        >
+          <AssignedComponent :order="order" :rates="conversionRates" />
+        </div>
+        <div
+          :class="`tab-pane fade ${show} ${active}`"
+          :id="`unassigned_${orderNo}`"
+          role="tabpanel"
+          v-if="showTab === `unassigned_${orderNo}`"
+        >
+          <UnassignedComponent :order="order" :rates="conversionRates" />
+        </div>
+        <div
+          :class="`tab-pane fade ${show} ${active}`"
+          :id="`create_${orderNo}`"
+          role="tabpanel"
+          v-if="showTab === `create_${orderNo}`"
+        >
+          <CreateOrderComponent :order="order" :rates="conversionRates" />
+        </div>
+      </div>
+    </div>
     <span class="badge badge-pill badge-primary rounded">Primary</span>
     <span class="badge badge-pill badge-secondary"
       ><AssignedComponent :order="order" :rates="conversionRates"
@@ -33,10 +115,16 @@ export default {
   },
   data() {
     return {
-      orderNo: this.orderno,
+      orderNo: '',
       order: null,
       errors: [],
       conversionRates: [],
+      showTab: `total_${this.order.order_details.order_no}`,
+      show: false,
+      active: false,
+      activetab: null,
+      firstShow: false,
+      firstActive: false,
     };
   },
   computed: {
@@ -62,6 +150,11 @@ export default {
 
     this.updateErrors(notification);
     this.updateNotification(actionClass);
+
+    this.firstShow = 'show';
+    this.firstActive = 'active';
+    this.orderNo = this.order.order_details.order_no;
+    const vendorTypeID = this.order.rider_details.vendor_type_id;
   },
   methods: {
     ...mapMutations({
@@ -74,9 +167,15 @@ export default {
       request_single_order: 'request_single_order',
       setExchangeRates: 'setExchangeRates',
     }),
+    viewTab(tab, orderNo) {
+      this.showTab = `${tab}_${orderNo}`;
+      this.active = 'active';
+      this.show = 'show';
+    },
     async singleOrderRequest() {
       try {
         const data = await this.request_single_order(this.orderNo);
+        console.log(data);
         return (this.order = data);
       } catch {
         this.errors.push(
