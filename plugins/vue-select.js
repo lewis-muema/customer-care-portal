b99755d1fef359760574a227f3cdcdffc4ee9be4
Clinton Mutindi <clinton@sendy.co.ke>
2019-12-12 20:21:03 +0300

    Update Plugin

--- plugins/vue-select.js
+++ plugins/vue-select.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
 import vSelect from 'vue-select';
-// import VCalendar from 'v-calendar';
-//
-// Vue.component('v-calender', VCalendar);
+
 Vue.component('v-select', vSelect);
