f2938aa35e07d82ca55c40c23fd53be4d445a422
lewism <lewis@sendy.co.ke>
2019-12-16 17:22:49 +0300

    Resolve Merge conflicts

--- modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/DashboardComponent.vue
+++ modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/DashboardComponent.vue
@@ -1,98 +1,23 @@
 <template>
-  <div id="tabs" class="container">
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-      <li class="nav-item">
-        <a
-          class="nav-link active"
-          :id="`total_${orderNo}`"
-          data-toggle="tab"
-          role="tab"
-          aria-selected="true"
-          @click="viewTab('total', orderNo)"
-          >Total</a
-        >
-      </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          :id="`assigned_${orderNo}`"
-          data-toggle="tab"
-          role="tab"
-          aria-selected="false"
-          @click="viewTab('assigned', orderNo)"
-          >Assigned</a
-        >
-      </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          :id="`unassigned_${orderNo}`"
-          data-toggle="tab"
-          role="tab"
-          aria-selected="false"
-          @click="viewTab('unassigned', orderNo)"
-          >Unassigned</a
-        >
-      </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          :id="`create_${orderNo}`"
-          data-toggle="tab"
-          role="tab"
-          aria-selected="false"
-          @click="viewTab('create', orderNo)"
-          >Create</a
-        >
-      </li>
-    </ul>
+  <td
+    colspan="9"
+    class="order_view_lower_cell cell-override"
+    :id="`order_view_lower${orderNo}`"
+    style="padding:0px; background-color: rgba(245, 245, 245, 0.56) !important; font-size: 13px;"
+  >
     <div>
-      <div>
-        <div
-          :class="`tab-pane fade ${firstShow} ${firstActive}`"
-          :id="`total_${orderNo}`"
-          role="tabpanel"
-          v-if="showTab === `total_${orderNo}`"
-        >
-          <AssignedComponent :order="order" :rates="conversionRates" />
-        </div>
-        <div
-          :class="`tab-pane fade ${show} ${active}`"
-          :id="`assigned_${orderNo}`"
-          role="tabpanel"
-          v-if="showTab === `assigned_${orderNo}`"
-        >
-          <AssignedComponent :order="order" :rates="conversionRates" />
-        </div>
-        <div
-          :class="`tab-pane fade ${show} ${active}`"
-          :id="`unassigned_${orderNo}`"
-          role="tabpanel"
-          v-if="showTab === `unassigned_${orderNo}`"
-        >
-          <UnassignedComponent :order="order" :rates="conversionRates" />
-        </div>
-        <div
-          :class="`tab-pane fade ${show} ${active}`"
-          :id="`create_${orderNo}`"
-          role="tabpanel"
-          v-if="showTab === `create_${orderNo}`"
-        >
-          <CreateOrderComponent :order="order" :rates="conversionRates" />
-        </div>
-      </div>
+      <!-- <span class="badge badge-pill badge-primary rounded">Primary</span> -->
+      <span class=""
+        ><AssignedComponent :order="order" :rates="conversionRates"
+      /></span>
+      <span class=""
+        ><CreateOrderComponent :order="order" :rates="conversionRates"
+      /></span>
+      <span class="badge badge-pill badge-success"
+        ><UnassignedComponent :order="order" :rates="conversionRates"
+      /></span>
     </div>
-    <span class="badge badge-pill badge-primary rounded">Primary</span>
-    <span class="badge badge-pill badge-secondary"
-      ><AssignedComponent :order="order" :rates="conversionRates"
-    /></span>
-    <span class="badge badge-pill badge-success"
-      ><UnassignedComponent :order="order" :rates="conversionRates"
-    /></span>
-    <span class="badge badge-pill badge-danger"
-      ><CreateOrderComponent :order="order" :rates="conversionRates"
-    /></span>
-  </div>
+  </td>
 </template>
 <script>
 import { mapGetters, mapMutations, mapActions } from 'vuex';
