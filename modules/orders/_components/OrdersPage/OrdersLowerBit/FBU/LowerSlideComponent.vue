026ed6f92f31199bfa5d1b2efc76fe3f41b0e3d5
lewism <lewis@sendy.co.ke>
2020-02-04 18:11:13 +0300

    Resolve styling discussions

--- modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/LowerSlideComponent.vue
+++ modules/orders/_components/OrdersPage/OrdersLowerBit/FBU/LowerSlideComponent.vue
@@ -1,9 +1,8 @@
 <template>
   <td
     colspan="9"
-    class="order_view_lower_cell"
+    class="order_view_lower_cell cell-override"
     :id="`order_view_lower${order.order_details.order_no}`"
-    style="padding:0px; background-color: rgba(245, 245, 245, 0.56) !important; font-size: 13px; width: 100vw;"
   >
     <TheNotificationsComponent :errors="errors" />
     <div class="lower_slide_bit" :id="`bumba_${order.order_details.order_no}`">
