e104341eae65c60708ef6068674bf81052070cca
lewism <lewis@sendy.co.ke>
2020-02-26 03:26:43 +0300

    Add fix conflicts in container pricing flow

--- mixins/pricing_configs_mixin.js
+++ mixins/pricing_configs_mixin.js
@@ -147,6 +147,7 @@ const PricingConfigsMxn = {
       } else if (model === 'Location') {
         this.pacInput1 = '';
         this.pacInput2 = '';
+        this.pacInput3 = '';
         const locationPricingRow = {
           id: 10,
           name: '',
@@ -164,11 +165,19 @@ const PricingConfigsMxn = {
             type: 'Point',
             coordinates: [39.671947, -4.056442],
           },
+          empty_return_location: {
+            type: 'Point',
+            coordinates: [37.671947, -4.056444],
+          },
           to: '',
-          status: 'Active',
+          empty_container_destination: '',
+          status: '',
           city: '',
-          order_amount: 23000,
-          rider_amount: 21400,
+          order_amount: 0,
+          rider_amount: 0,
+          container_weight_tonnes: '',
+          container_size_feet: '',
+          container_errand_type: 'drop_off',
         };
         this.tableData.push(locationPricingRow);
       }
