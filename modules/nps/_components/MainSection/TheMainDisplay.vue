b2e1445a79f79d2e467dc219976f4c2e3039c04a
joyce.k <joyce@sendy.co.ke>
2020-03-25 10:28:51 +0300

    single user survey history

--- modules/nps/_components/MainSection/TheMainDisplay.vue
+++ modules/nps/_components/MainSection/TheMainDisplay.vue
@@ -25,54 +25,8 @@
           </button>
         </div>
       </div>
-      <div class="row survey" :key="surveyComponentKey">
-        <div v-if="!returned" class="survey-info">
-          <div class="text-center">
-            <i class="fa fa-spinner fa-spin loader"></i>
-          </div>
-        </div>
-        <div v-if="returned && surveyData.length === 0" class="survey-info">
-          <div class="alert alert-info text-center">
-            <strong> {{ noDataMsg }}</strong>
-          </div>
-        </div>
-
-        <div
-          v-for="(survey, index) in surveyData"
-          :key="index"
-          class="survey-info"
-          v-else
-        >
-          <div :class="`row group-border border_${survey.respondent_group} `">
-            <div :class="`col-md-1 score-display`">
-              <div :class="`score ${survey.respondent_group}`">
-                {{ survey.score }}
-              </div>
-            </div>
-            <div class="col-md-10">
-              <div class="survey-top">
-                <span
-                  class="survey-header"
-                  @click="
-                    view_user(survey.respondent_id, survey.respondent_type)
-                  "
-                >
-                  <nuxt-link class="survey-header" to="/nps/user">{{
-                    survey.respondent_name
-                  }}</nuxt-link></span
-                >
-                <span class="survey-period pull-right">
-                  {{ getTimeFromNow(survey.surveyed_on) }}
-                </span>
-              </div>
-
-              <div class="survey-body">{{ survey.comments }}</div>
-            </div>
-          </div>
-          <div class="tag-holder row">
-            Add tag
-          </div>
-        </div>
+      <div>
+        <TheSurveyComponent :survey-data="surveyData" :returned="returned" />
       </div>
     </div>
     <div class="row" v-if="returned && surveyData.length !== 0">
@@ -112,6 +66,8 @@ export default {
   components: {
     TheDatePickerComponent: () =>
       import('~/modules/nps/_components/TheDatePicker'),
+    TheSurveyComponent: () =>
+      import('~/modules/nps/_components/MainSection/TheSurveyComponent'),
   },
   props: {
     surveys: {
@@ -395,83 +351,6 @@ export default {
   background: #093359;
   color: #ffffff;
 }
-.survey {
-  margin-top: 20px;
-  margin-top: 20px;
-  height: 500px;
-  overflow-y: auto;
-  padding-top: 23px;
-}
-.survey-info {
-  width: 97%;
-  margin-bottom: 20px;
-  border-radius: 5px;
-  margin-left: 18px;
-  box-shadow: 0 0 1em #e6e7e8;
-}
-.bottom-pages {
-  margin-left: 75%;
-  flex: none;
-  width: auto;
-}
-.score {
-  padding-top: 2px;
-  width: 37px;
-  border-radius: 100px;
-  color: #ffffff;
-  font-weight: 700;
-  font-size: 21px;
-  height: 37px;
-  text-align: center;
-}
-.passive {
-  background: #6b8399;
-}
-.promoter {
-  background: #21a642;
-}
-.detractor {
-  background: #d9372b;
-}
-.score-display {
-  margin-top: auto;
-  margin-bottom: auto;
-}
-
-.group-border {
-  overflow-y: auto;
-  width: 100%;
-  border: 1px solid #ebf1f7;
-  margin-right: 0;
-  margin-left: 0;
-}
-.tag-holder {
-  padding: 9px 10px;
-  border: none;
-  width: 100%;
-  color: #000000;
-  margin-right: 0;
-  margin-left: 0;
-  background: #ecf0f5;
-}
-.survey-top {
-  padding: 5px 0;
-}
-.survey-body {
-  margin-bottom: 23px;
-}
-.survey-header {
-  font-size: 17px;
-  color: #f57f20;
-  font-weight: 600;
-  cursor: pointer;
-}
-.survey-period {
-  color: #000000;
-  font-size: 14px;
-  line-height: 24px;
-  font-weight: 600;
-}
 .previous {
   -webkit-transform: translateY(-2px) rotate(130deg);
 }
@@ -494,5 +373,10 @@ export default {
 .pages-number {
   background: #ffffff;
   border-right: none;
+}
+.bottom-pages {
+  margin-left: 75%;
+  flex: none;
+  width: auto;
 }
 </style>
--- modules/nps/_components/UserSection/TheUserView.vue
+++ modules/nps/_components/UserSection/TheUserView.vue
@@ -1,28 +1,123 @@
 <template>
   <div class="box box-info">
     <div class="box-body nps-holder">
-      <div class="nps-back-header">
-        <nuxt-link class="nps-back-header row" to="/nps/dashboard">
-          <div class="col-md-5">
-            <span
-              class="accordion-item-trigger-icon back-icon pull-left"
-            ></span>
-            <span class="back-text"> Back to Dashboard</span>
+      <div v-if="surveys === null">
+        <div class="text-center">
+          Loading User dashboard ....
+          <i class="fa fa-spinner fa-spin loader"></i>
+        </div>
+      </div>
+      <div v-else class="nps-user-holder">
+        <div class="nps-back-header">
+          <nuxt-link class="nps-back-header" to="/nps/dashboard">
+            <div class="col-md-5">
+              <span
+                class="accordion-item-trigger-icon back-icon pull-left"
+              ></span>
+              <span class="back-text"> Back to Dashboard</span>
+            </div>
+          </nuxt-link>
+        </div>
+        <div v-if="returned && surveys.length === 0" class="survey-info">
+          <div class="alert alert-info text-center">
+            <strong> {{ noDataMsg }}</strong>
           </div>
-        </nuxt-link>
+        </div>
+        <div v-else>
+          <div class="metrics-header">
+            <div class="col-md-6 metrics-text">
+              {{ userName }} ( {{ userType }} )
+            </div>
+          </div>
+          <div class="row user-main">
+            <div class="col-md-3">
+              <TheUserSidebarComponent :meta="metaData" :user-type="userType" />
+            </div>
+            <div class="col-md-8">
+              <TheUserMainComponent :surveys="surveys" :returned="returned" />
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="row metrics-header">
-        <div class="col-md-6 metrics-text">James Mbuthia Muriuki</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-3">sidebar</div>
-      <div class="col-md-8">main bar</div>
     </div>
   </div>
 </template>
 <script>
+import { mapGetters, mapMutations, mapActions, mapState } from 'vuex';
+
 export default {
   name: 'TheUserView',
+  components: {
+    TheUserSidebarComponent: () =>
+      import('~/modules/nps/_components/UserSection/TheUserSidebar'),
+    TheUserMainComponent: () =>
+      import('~/modules/nps/_components/UserSection/TheUserMainSection'),
+  },
+  data() {
+    return {
+      metaData: null,
+      npsRequestStatus: false,
+      surveys: null,
+      returned: false,
+      noDataMsg: 'There are no surveys recorded for this user',
+    };
+  },
+  computed: {
+    ...mapGetters(['getSurveys', 'getCurrentNPSPage']),
+
+    userType() {
+      return this.$route.query.respondent_type;
+    },
+    userID() {
+      return this.$route.query.respondent_id;
+    },
+    userName() {
+      const surveys = this.surveys;
+      return surveys !== null && surveys.length !== 0
+        ? surveys[0].respondent_name
+        : 'Not Set';
+    },
+    lastSurveyDate() {
+      const surveys = this.surveys;
+      return surveys !== null && surveys.length !== 0
+        ? surveys[0].surveyed_on
+        : 'Not Set';
+    },
+  },
+  watch: {
+    getSurveys(surveyData) {
+      const metaData = surveyData.pagination;
+      this.surveys = surveyData.data;
+      metaData.lastSurveyDate = this.lastSurveyDate;
+      this.metaData = metaData;
+      this.returned = true;
+    },
+  },
+  mounted() {
+    if (process.client) {
+      this.setSurveys({
+        page: 1,
+        params: {
+          dismissed: 0,
+          respondent_type: this.userType,
+          respondent_id: this.userID,
+        },
+      });
+    }
+  },
+  methods: {
+    ...mapActions(['setSurveys']),
+  },
 };
 </script>
+<style scoped>
+.nps-user-holder {
+  padding-left: 15px;
+}
+.metrics-header {
+  margin-bottom: 20px;
+}
+.user-main {
+  margin-left: 0;
+}
+</style>
