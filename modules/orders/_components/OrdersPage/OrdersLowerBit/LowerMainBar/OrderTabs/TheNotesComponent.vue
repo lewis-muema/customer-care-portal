e600c8662efda6e6cdcbcd9c5f79a4a1d310f38d
Clinton Mutindi <clinton@sendy.co.ke>
2020-07-08 17:32:30 +0300

    Notes : Additional Instructions

--- modules/orders/_components/OrdersPage/OrdersLowerBit/LowerMainBar/OrderTabs/TheNotesComponent.vue
+++ modules/orders/_components/OrdersPage/OrdersLowerBit/LowerMainBar/OrderTabs/TheNotesComponent.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-use-v-if-with-v-for,vue/no-confusing-v-for-v-if -->
 <template>
   <div class="post">
     <div class="user-block">
@@ -15,7 +16,76 @@
       <span class="description"> </span>
     </div>
     <p v-if="!notesLog.length">No notes found</p>
-    <p v-for="note in notesLog" :key="note.index">{{ note.msg }}</p>
+    <div v-else>
+      <div v-if="notesLog[0].msg === 'none'">
+        <div class="">
+          <div class="route-header">
+            PICKUP INSTRUCTIONS AT {{ notesData()[0].name }}
+          </div>
+          <div v-if="checkPickUpNotes(notesData()[0])">
+            <div v-if="displayNotes(notesData()[0])" class="notes-text">
+              <div>
+                {{ notesData()[0].msg }}
+              </div>
+            </div>
+
+            <div
+              v-if="notesData()[0].recipient_phone !== null"
+              class="additional-instructions-content additional-instructions-wrapper"
+            >
+              <div class="additional-instructions__flex">
+                <i
+                  class=" el-icon-phone-outline additional-instructions__image recipient_contact-icon"
+                />
+              </div>
+              <div class="additional-notes-outer">
+                <div class="additional-notes-recipient">
+                  {{ notesData()[0].recipient_phone }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="" v-else>
+            No notes provided.
+          </div>
+        </div>
+
+        <div v-for="(val, index) in notesData()" v-if="index > 0" :key="index">
+          <div class="route-header">
+            DROP OFF INSTRUCTIONS AT {{ val.name }}
+          </div>
+          <div v-if="checkPickUpNotes(val)">
+            <div v-if="displayNotes(val)" class="notes-text">
+              <div>
+                {{ val.msg }}
+              </div>
+            </div>
+
+            <div
+              v-if="val.recipient_phone !== null"
+              class="additional-instructions-content additional-instructions-wrapper"
+            >
+              <div class="additional-instructions__flex">
+                <i
+                  class=" el-icon-phone-outline additional-instructions__image recipient_contact-icon"
+                />
+              </div>
+              <div class="additional-notes-outer">
+                <div class="additional-notes-recipient">
+                  {{ val.recipient_phone }}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="" v-else>
+            No notes provided.
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        {{ notesLog[0].msg }}
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -36,5 +106,70 @@ export default {
       notesLog: this.order,
     };
   },
+  mounted() {},
+  methods: {
+    notesData() {
+      return this.notesLog.slice(1);
+    },
+    checkPickUpNotes(val) {
+      let resp = true;
+      if (
+        val.recipient_phone === null &&
+        (Object.prototype.hasOwnProperty.call(val, 'msg') === false ||
+          val.msg === null)
+      ) {
+        resp = false;
+      }
+      return resp;
+    },
+    displayNotes(val) {
+      const resp = true;
+      if (
+        val.msg === null ||
+        Object.prototype.hasOwnProperty.call(val, 'msg') === false
+      ) {
+        return false;
+      }
+      return resp;
+    },
+  },
 };
 </script>
+<style media="screen">
+.additional-instructions__flex {
+  flex-basis: 4%;
+}
+.additional-instructions__image {
+  height: 30px;
+  min-width: 30px;
+}
+.recipient_contact-icon {
+  font-size: 23px;
+}
+.additional-notes-outer {
+  flex-direction: column;
+  flex: 1;
+}
+.additional-notes-recipient {
+  padding: 3px;
+}
+.additional-instructions-wrapper {
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-basis: 43%;
+}
+.additional-instructions-content {
+  justify-content: space-between;
+  align-items: center;
+  display: flex;
+  margin-top: 1%;
+}
+.notes-text {
+  margin-top: 1%;
+}
+.route-header {
+  color: #000;
+  font-size: 13px;
+}
+</style>
