adaef651aa3877f4708256fc1bbdd62851b7fa1c
Clinton Mutindi <clinton@sendy.co.ke>
2021-01-18 12:14:49 +0300

    Freight : Status Approval
    - Restrict user to updating freight status for verified accounts only.

--- store/actions.js
+++ store/actions.js
@@ -1635,4 +1635,24 @@ export default {
       return error.response;
     }
   },
+  async request_user_freight_status({ state, dispatch }, payload) {
+    const config = state.config;
+    const jwtToken = localStorage.getItem('jwtToken');
+    const param = {
+      headers: {
+        'Content-Type': 'text/plain',
+        Accept: 'application/json',
+        Authorization: jwtToken,
+      },
+    };
+    const url = `${config.ADONIS_API}freight-status?${payload.val}`;
+    try {
+      const response = await axios.get(url, param);
+      return response.data;
+    } catch (error) {
+      const err = await dispatch('handleErrors', error.response.status, {
+        root: true,
+      });
+    }
+  },
 };
