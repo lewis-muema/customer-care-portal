e75f8fc2f5688723719df1d384b3af67e73af029
shelef ndar <shelef@sendy.co.ke>
2021-01-25 19:28:55 +0300

    apm handle.

--- config/config.js
+++ config/config.js
@@ -38,7 +38,7 @@ const config = {
     BROKER_PASS: '0FAHmQQmjfsIXdro',
 
     ELASTIC_APM_DISTRIBUTED_TRACING_ORIGINS: 'https://authtest.sendyit.com',
-    ELASTIC_APM_SERVER_URL: 'http://localhost:8200',
+    ELASTIC_APM_SERVER_URL: 'https://staging-elk.sendyit.com:8200',
     ELASTIC_APM_ENVIRONMENT: 'dev',
     ELASTIC_APM_SERVICE_VERSION: packageJson.version,
   },
@@ -77,7 +77,7 @@ const config = {
     BROKER_PASS: '0FAHmQQmjfsIXdro',
 
     ELASTIC_APM_DISTRIBUTED_TRACING_ORIGINS: 'https://authtest.sendyit.com',
-    ELASTIC_APM_SERVER_URL: 'https://staging-apm.sendyit.com:443',
+    ELASTIC_APM_SERVER_URL: 'https://staging-elk.sendyit.com:8200',
     ELASTIC_APM_ENVIRONMENT: 'staging',
     ELASTIC_APM_SERVICE_VERSION: packageJson.version,
   },
@@ -113,6 +113,11 @@ const config = {
       'wss://rabbitmqtest.sendyit.com:8443/ws?apikey=4RNNeyATKN6B6S6XiOyJdPMEJ3oLRKBT',
     BROKER_USER: 'staging',
     BROKER_PASS: '0FAHmQQmjfsIXdro',
+
+    ELASTIC_APM_DISTRIBUTED_TRACING_ORIGINS: 'https://authtest.sendyit.com',
+    ELASTIC_APM_SERVER_URL: 'https://staging-elk.sendyit.com:8200',
+    ELASTIC_APM_ENVIRONMENT: 'staging',
+    ELASTIC_APM_SERVICE_VERSION: packageJson.version,
   },
   production: {
     BASE_URL: 'http://localhost:8080/',
