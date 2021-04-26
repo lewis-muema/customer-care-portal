b95b344fc7336aa616109129c9471f8897ed17cd
Caleb Nasio <caleb@sendyit.com>
2021-04-26 10:52:49 +0300

    added cancellation reason to cancellation status edit function

--- modules/rewards/_components/CancellationReasonsDataTables/ActiveCancellationsDataTable.vue
+++ modules/rewards/_components/CancellationReasonsDataTables/ActiveCancellationsDataTable.vue
@@ -189,6 +189,7 @@ export default {
         admin_id: this.getSession.payload.data.admin_id,
         status: row.status === 1 ? 2 : 1,
         cancellation_reason_id: row.id,
+        cancel_reason: row.cancellation_reason,
         country_filter: countryCode,
       };
       await this.update_cancellation_status(payload);
