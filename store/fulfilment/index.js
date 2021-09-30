c4fc5cfd131f8f90ae49914f90bffe4a002b5361
Clinton Mutindi <clinton@sendy.co.ke>
2021-09-30 09:45:38 +0300

    - Add fulfilment store

---
+++ store/fulfilment/index.js
@@ -0,0 +1,7 @@
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
+
+const state = () => ({});
+
+export { state, getters, mutations, actions };
