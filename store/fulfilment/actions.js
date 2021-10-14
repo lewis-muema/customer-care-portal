b31022f1b3d15cd98c54ed096a394fa991fb7d0d
Clinton Mutindi <clinton@sendy.co.ke>
2021-10-14 12:06:46 +0300

    Hubs
    - List Hubs details

--- store/fulfilment/actions.js
+++ store/fulfilment/actions.js
@@ -1,4 +1,5 @@
 import FulfilmentData from '../../mixins/fulfilment_data';
+import { axiosConfig } from '~/middleware/axios';
 
 export default {
   setTableProps({ commit }, payload) {
@@ -185,25 +186,17 @@ export default {
     }, 1000);
   },
 
-  async getHubList({ commit }, payload) {
+  async getHubList({ rootState, commit, dispatch }) {
     commit('setProcessingStatus', true);
-    const promise = new Promise(resolve => {
-      const response = {
-        pagination: {
-          total: 987,
-          perPage: 50,
-          page: 1,
-          lastPage: 20,
-        },
-        data: FulfilmentData.hubs,
-      };
-      resolve(response);
-    });
+    const config = rootState.config;
+    const url = `${config.FULFILMENT_SERVICE}missioncontrol/hubs`;
 
-    const results = await promise;
+    const results = await axiosConfig.get(url);
     setTimeout(() => {
-      commit('setTableData', results.data);
-      commit('setPagination', results.pagination);
+      const res = results.data;
+      console.log('results', results);
+      commit('setTableData', res.data);
+      // commit('setPagination', results.pagination);
       commit('setProcessingStatus', false);
     }, 1000);
   },
