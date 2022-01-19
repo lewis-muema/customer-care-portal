b4dd15cae35dc335b81f9883371b5286b3e14f33
Clinton <clinton@sendy.co.ke>
2022-01-19 11:57:44 +0300

    Failed Request : Cancel Order
    - Cancel failed request order table action UI.

--- modules/fulfilment/globals/_components/actions/CancelOrder.vue
+++ modules/fulfilment/globals/_components/actions/CancelOrder.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>cancel order</div>
+  <div>
+    <div class="cancel-batch-dialog cancel-batch-action-section">
+      <div class="batch-input-label batch-cancel-label">
+        Why do you want to cancel?
+      </div>
+      <el-input
+        type="textarea"
+        class="batch-cancellation-reason"
+        :rows="2"
+        placeholder="Please input"
+        v-model="cancel_reason"
+      >
+      </el-input>
+
+      <el-button
+        type="danger"
+        class="action-submit-button"
+        :class="
+          !cancel_reason ? 'disabled-batch-order-btn' : 'cancel-batch-order-btn'
+        "
+        :disabled="!cancel_reason"
+      >
+        Cancel order
+      </el-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Cancel',
+  data() {
+    return {
+      cancel_reason: '',
+    };
+  },
 };
 </script>
 
-<style></style>
+<style lang="css" scoped></style>
