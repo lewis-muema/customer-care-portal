5813059e77b73cfa0e7bacb639fe51ac4a6703bd
Clinton <clinton@sendy.co.ke>
2022-02-10 13:30:47 +0300

    Mission Control : Seller Stats data integration
    - Integrate with seller stats endpoint

--- store/fulfilment/actions.js
+++ store/fulfilment/actions.js
@@ -693,4 +693,17 @@ export default {
       return error.response.data.data;
     }
   },
+  async getSellerStats({ rootState, commit }, payload) {
+    const url = rootState.config.AUTH;
+    try {
+      const response = await axiosConfig.get(
+        `${url}mission-control-bff/sellers/summary`,
+      );
+      if (response.status === 200) {
+        return response.data.data;
+      }
+    } catch (error) {
+      return error.response.data.data;
+    }
+  },
 };
